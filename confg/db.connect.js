ba563916c9e2c7a87271aff1c1b50a8047f9ab4b
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-04-03 20:32:41 +0530

    feat: fixing error while deploying

--- confg/db.connect.js
+++ confg/db.connect.js
@@ -2,6 +2,18 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const connectdatabase = mongoose.connect(process.env.mongourl)
+const connectdatabase = async () => {
+    try {
+      await mongoose.connect(process.env.mongourl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`MongoDB connected with server ${mongoose.connection.host}`);
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    }
+  };
+
 
 module.exports=connectdatabase
